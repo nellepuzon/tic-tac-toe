8f49091e209c2413f0c91713ba16df5ed43435cf
Junelle Puzon <nellepuzon@gmail.com>
2022-02-28 16:03:34 +0800

    x turn first

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,13 @@ const titleContainer = document.querySelector(".title-container");
 const playButton = document.querySelector(".play-button");
 const gameContainer = document.querySelector(".container");
 
+
+let oTurn;
+
 playButton.addEventListener("click", startGame);
 
 function startGame() {
+    oTurn = false;
     gameContainer.classList.add("show-board");
     titleContainer.classList.add("hide");
     topShapes.classList.add("hide-shapes");
