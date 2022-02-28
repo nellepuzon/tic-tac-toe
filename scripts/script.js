0dabc41a192da4eeaf53f0b91dbe03e3f53923ef
Junelle Puzon <nellepuzon@gmail.com>
2022-02-28 16:36:52 +0800

    place x on cell

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ const bottomShapes = document.querySelector(".bottom-decor");
 const titleContainer = document.querySelector(".title-container");
 const playButton = document.querySelector(".play-button");
 const gameContainer = document.querySelector(".container");
+const letterX = "x";
+const letterO = "o";
+const cells = document.querySelectorAll(".cell");
 
 
 let oTurn;
@@ -10,11 +13,33 @@ let oTurn;
 playButton.addEventListener("click", startGame);
 
 function startGame() {
-    oTurn = false;
     gameContainer.classList.add("show-board");
     titleContainer.classList.add("hide");
     topShapes.classList.add("hide-shapes");
     bottomShapes.classList.add("hide-shapes");
+    oTurn = false;
+    cells.forEach((cell) => {
+        cell.classList.remove(letterX);
+        cell.classList.remove(letterO);
+        cell.addEventListener("click", cellClick, { once: true });
+});
 }
 
+function cellClick(e) {
+    const cell = e.target;
+    const playerTurn = oTurn ? letterO : letterX;
+    placeLetter(cell, playerTurn);
+    if (checkWin(playerTurn)) {
+      endGame(false);
+    } else if (isDraw()) {
+      endGame(true);
+    } else {
+      swapTurns();
+      cellHover();
+    }
+  }
+
+  function placeLetter(cell, playerTurn) {
+    cell.classList.add(playerTurn);
+  }
 
