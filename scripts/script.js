1836def5d65ad7290a1126ac5bf29705351e13e6
Junelle Puzon <nellepuzon@gmail.com>
2022-02-28 17:18:15 +0800

    check winner

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,16 @@ const letterX = "x";
 const letterO = "o";
 const cells = document.querySelectorAll(".cell");
 const board = document.querySelector(".board");
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
 let oTurn;
 
@@ -18,25 +28,27 @@ function startGame() {
   topShapes.classList.add("hide-shapes");
   bottomShapes.classList.add("hide-shapes");
   oTurn = false;
+  cells.forEach((cell) => {
+    cell.classList.remove(letterX);
+    cell.classList.remove(letterO);
+    cell.addEventListener("click", cellClick, { once: true });
+  });
   cellHover();
 }
-
-cells.forEach((cell) => {
-  cell.classList.remove(letterX);
-  cell.classList.remove(letterO);
-  cell.addEventListener("click", cellClick, { once: true });
-});
 
 function cellClick(e) {
   const cell = e.target;
-  const playerTurn = oTurn ? letterO : letterX;
-  placeLetter(cell, playerTurn);
+  const currentPlayer = oTurn ? letterO : letterX;
+  placeLetter(cell, currentPlayer);
   swapTurns();
   cellHover();
+  if (checkWin(currentPlayer)) {
+      console.log('winner')
+  }
 }
 
-function placeLetter(cell, playerTurn) {
-  cell.classList.add(playerTurn);
+function placeLetter(cell, currentPlayer) {
+  cell.classList.add(currentPlayer);
 }
 
 function swapTurns() {
@@ -52,3 +64,12 @@ function cellHover() {
     board.classList.add(letterX);
   }
 }
+
+function checkWin(currentPlayer) {
+    return winningCombinations.some((combination) => {
+      return combination.every((index) => {
+        return cells[index].classList.contains(currentPlayer);
+      });
+    });
+  }
+  
