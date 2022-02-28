6c1317dedc6bd37d3470309e8f07a7d3bd2510e4
Junelle Puzon <nellepuzon@gmail.com>
2022-02-28 16:46:01 +0800

    place o on cell

--- scripts/script.js
+++ scripts/script.js
@@ -7,39 +7,39 @@ const letterX = "x";
 const letterO = "o";
 const cells = document.querySelectorAll(".cell");
 
-
 let oTurn;
 
 playButton.addEventListener("click", startGame);
 
 function startGame() {
-    gameContainer.classList.add("show-board");
-    titleContainer.classList.add("hide");
-    topShapes.classList.add("hide-shapes");
-    bottomShapes.classList.add("hide-shapes");
-    oTurn = false;
-    cells.forEach((cell) => {
-        cell.classList.remove(letterX);
-        cell.classList.remove(letterO);
-        cell.addEventListener("click", cellClick, { once: true });
-});
+  gameContainer.classList.add("show-board");
+  titleContainer.classList.add("hide");
+  topShapes.classList.add("hide-shapes");
+  bottomShapes.classList.add("hide-shapes");
+  oTurn = false;
 }
 
+cells.forEach((cell) => {
+    cell.classList.remove(letterX);
+    cell.classList.remove(letterO);
+    cell.addEventListener("click", cellClick, { once: true });
+  });
+
+
 function cellClick(e) {
-    const cell = e.target;
-    const playerTurn = oTurn ? letterO : letterX;
-    placeLetter(cell, playerTurn);
-    if (checkWin(playerTurn)) {
-      endGame(false);
-    } else if (isDraw()) {
-      endGame(true);
-    } else {
-      swapTurns();
-      cellHover();
-    }
+  const cell = e.target;
+  const playerTurn = oTurn ? letterO : letterX;
+  placeLetter(cell, playerTurn);
+    swapTurns();
+    cellHover();
   }
 
-  function placeLetter(cell, playerTurn) {
-    cell.classList.add(playerTurn);
-  }
+
+function placeLetter(cell, playerTurn) {
+  cell.classList.add(playerTurn);
+}
+
+function swapTurns() {
+  oTurn = !oTurn;
+}
 
